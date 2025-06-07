3fcccd4aa6951a27b53efde35027ea38b6936bc3
Ryosuke Iwanaga <riywo@opsbr.com>
2025-06-06 22:39:39 -0700

    wip

---
+++ chapter1/chapter1.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import torch
+import torch.nn as nn
+from transformers import AutoTokenizer
+
+torch.classes.__path__ = []
+
+with st.echo():
+
+    model_name = "Qwen/Qwen3-0.6B"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Vocaburary size is the number of unique tokens in the tokenizer.
+    # The second term is for the special tokens like <|endoftext|>.
+    vocab_size = tokenizer.vocab_size + len(tokenizer.added_tokens_decoder)
+
+
+st.text(f"{vocab_size = :,}")
+
+input_text = st.text_input("input_text", "Hello, world!")
+
+st.write(input_text)
