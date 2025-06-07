784f6821c9eb15bc5b6356b43d185bae103ef99c
Ryosuke Iwanaga <me@riywo.com>
2025-06-06 22:56:53 -0700

    Refactor to display and run logic via function

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -2,11 +2,12 @@ import streamlit as st
 import torch
 import torch.nn as nn
 from transformers import AutoTokenizer
+import inspect
 
 torch.classes.__path__ = []
 
-with st.echo():
 
+def demo():
     model_name = "Qwen/Qwen3-0.6B"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
@@ -14,9 +15,13 @@ with st.echo():
     # The second term is for the special tokens like <|endoftext|>.
     vocab_size = tokenizer.vocab_size + len(tokenizer.added_tokens_decoder)
 
+    st.text(f"{vocab_size = :,}")
 
-st.text(f"{vocab_size = :,}")
+    input_text = st.text_input("input_text", "Hello, world!")
 
-input_text = st.text_input("input_text", "Hello, world!")
+    st.write(input_text)
 
-st.write(input_text)
+
+st.code(inspect.getsource(demo), language="python")
+
+demo()
